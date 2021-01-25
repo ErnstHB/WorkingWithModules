b6cc9001b4165d73351ae577cd544eba27cade16
Ernesto <66950087+ErnstHB@users.noreply.github.com>
2021-01-25 23:27:10 +0100

    Working with modules course updates

---
+++ public/js/sessionRepository.js
@@ -0,0 +1,19 @@
+const sessionsURL = 'sessions.json';
+
+let sessionList = [];
+
+function getSessions(){
+    return new Promise(function(resolve, reject){
+      var oReq = new XMLHttpRequest();
+      oReq.onload = function (e) {
+          sessionList = e.target.response;
+        resolve(sessionList);
+      };
+      oReq.open('GET', sessionsURL, true);
+      oReq.responseType = 'json';
+      oReq.send();
+    })
+  }
+
+export {getSessions as default, sessionsURL};
+export {sessionTemplate} from './template.js'
